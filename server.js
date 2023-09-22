a645ab791c122e6950e7e368cc9207a1ee3b13f4
Guy Jara <guy_jara@yahoo.com>
2023-09-22 13:31:36 -0700

    Dependencies installed, basic code in server.js and .env file. Server runs, but db connectivity is not validated.

---
+++ server.js
@@ -0,0 +1,77 @@
+/* Require modules 
+
+--------------------------------------------------------------- */
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const methodOverride = require('method-override'); 
+
+/* Require the db connection, models, and seed data 
+--------------------------------------------------------------- */ 
+
+// const db = require('./models'); 
+
+ 
+ 
+
+/* Require the routes in the controllers folder
+--------------------------------------------------------------- */ 
+
+ 
+ 
+ 
+
+/* Create the Express app
+--------------------------------------------------------------- */
+const app = express(); 
+
+
+/* Configure the app to refresh the browser when nodemon restarts
+--------------------------------------------------------------- */ 
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    // wait for nodemon to fully restart before refreshing the page
+    setTimeout(() => { 
+        liveReloadServer.refresh("/");
+    }, 100);
+}); 
+
+ 
+ 
+
+/* Configure the app (app.set)
+--------------------------------------------------------------- */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+ 
+
+/* Middleware (app.use)
+--------------------------------------------------------------- */
+app.use(express.static('public'));
+app.use(connectLiveReload()); 
+
+// Body parser: used for POST/PUT/PATCH routes:
+// this will take incoming strings from the body that are URL encoded and parse them  
+// into an object that can be accessed in the request parameter as a property called body (req.body). 
+app.use(express.urlencoded({ extended: true }));
+// Allows us to interpret POST requests from the browser as another request type: DELETE, PUT, etc. 
+app.use(methodOverride('_method')); 
+
+/* Mount routes
+--------------------------------------------------------------- */ 
+
+// HOME Route
+app.get('/', function (req, res) {
+    res.send('To-do Manager');
+}); 
+
+ 
+
+/* Tell the app to listen on the specified port
+--------------------------------------------------------------- */
+console.log(process.env.PORT);
+app.listen(process.env.PORT, function () {
+    console.log('Express is listening to port', process.env.PORT);
+}); 
