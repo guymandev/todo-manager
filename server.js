2b4a313dc01740214964401730e7eedad16600c8
Guy Jara <guy_jara@yahoo.com>
2023-09-28 12:17:22 -0700

    Fixed styling of Create Todo button.

--- server.js
+++ server.js
@@ -23,13 +23,26 @@ const app = express();
 
 /* Configure the app to refresh the browser when nodemon restarts
 --------------------------------------------------------------- */ 
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-    // wait for nodemon to fully restart before refreshing the page
-    setTimeout(() => { 
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.server.once("connection", () => {
+//     // wait for nodemon to fully restart before refreshing the page
+//     setTimeout(() => { 
+//         liveReloadServer.refresh("/");
+//     }, 100);
+// }); 
+
+// Detect if running in a dev environment
+if (process.env.ON_HEROKU === 'false') {
+    // Configure the app to refresh the browser when nodemon restarts
+    const liveReloadServer = livereload.createServer();
+    liveReloadServer.server.once("connection", () => {
+        // wait for nodemon to fully restart before refreshing the page
+        setTimeout(() => {
         liveReloadServer.refresh("/");
-    }, 100);
-}); 
+        }, 100);
+    });
+    app.use(connectLiveReload());
+};
 
 /* Configure the app (app.set)
 --------------------------------------------------------------- */
@@ -40,7 +53,10 @@ app.set('views', path.join(__dirname, 'views'));
 /* Middleware (app.use)
 --------------------------------------------------------------- */
 app.use(express.static('public'));
-app.use(connectLiveReload());
+// Comment out the connectLiveReload, because it's now happeing
+// above.
+// app.use(connectLiveReload());
+
 // Body parser: used for POST/PUT/PATCH routes:
 // this will take incoming strings from the body that are URL encoded and parse them  
 // into an object that can be accessed in the request parameter as a property called body (req.body). 
